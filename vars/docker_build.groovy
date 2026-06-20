ceb5311f916569a836289768e2ace2ab7cb0b42c
Sunnny charkhwal <sunny.charkhwal@gmail.com>
2026-06-20 13:05:50 +0530

    Refactor docker_build function and add .dockerignore entries
    Refactor docker_build function to accept image name and build context as parameters. Add .dockerignore entries to prevent permission errors during build.

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,12 +1,11 @@
-def call(Map config = [:]) {
-    def imageName = config.imageName ?: error("Image name is required")
-    def imageTag = config.imageTag ?: 'latest'
-    def dockerfile = config.dockerfile ?: 'Dockerfile'
-    def context = config.context ?: '.'
+def call(String imageName, String buildContext = '.') {
+    // Dynamically adding common problem directories to .dockerignore 
+    // to prevent permission denied errors during the Docker build context upload.
+    sh '''
+        echo "mysql-data/" >> .dockerignore
+        echo ".git/" >> .dockerignore
+    '''
     
-    echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
-    
-    sh """
-        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
-    """
+    // Build the Docker image using the provided name and context
+    sh "docker build -t ${imageName}:latest ${buildContext}"
 }
